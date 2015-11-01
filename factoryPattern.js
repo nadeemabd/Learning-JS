2189692e894b3f0d9e5b02634d797ba3b5af3e30
Nadeem Abdulla <nadeemabd@gmail.com>
2015-11-01 21:46:28 +0530

    first commit

---
+++ factoryPattern.js
@@ -0,0 +1,22 @@
+﻿function createPerson(firstName, lastName) {
+    var person = {
+
+        get firstName() {
+            return firstName;
+        },
+        get lastName() {
+            return lastName;
+        },
+        greet: function (name) {
+            return "Hello, " + name + ". I am " + firstName + " " + lastName;
+        }
+    };
+
+    return person;
+}
+
+var obj = createPerson("John", "Doe");
+var obj2 = createPerson("Joe", "Smith");
+
+console.log(obj.greet("Jane"));
+console.log(obj2.greet("Jane2"));
